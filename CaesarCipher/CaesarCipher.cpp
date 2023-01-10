426ff7f6b02350d3c0938a57bc9757837f860040
MaksMN <maksimovmn78@gmail.com>
2023-01-10 10:04:58 +0300

    Релиз

--- CaesarCipher/CaesarCipher.cpp
+++ CaesarCipher/CaesarCipher.cpp
@@ -4,21 +4,20 @@ using namespace std;
 int main() {
 	setlocale(LC_ALL, "");
 	do {
-		char cipherOperation;
+		char operation;
 		cout << "Выберите операцию (e - шифрование, d - дешифрование, q - выход): ";
-		cin >> cipherOperation;
-		if ((cipherOperation != 'e') && (cipherOperation != 'd') && (cipherOperation != 'q')) { cout << "Неверный ввод!\n"; continue; }
-		if (cipherOperation == 'q') { cout << "Goodbye"; return 0; }
-
-		int strlen;
+		cin >> operation;
+		if ((operation != 'e') && (operation != 'd') && (operation != 'q')) { cout << "Неверный ввод!\n"; continue; }
+		if (operation == 'q') { cout << "Goodbye"; return 0; }
+		
+		int strlen , offset;
 		cout << "Введите длину строки: "; cin >> strlen;
 		char* str = new char[strlen + 1];
 		cout << "Введите строку: ";	cin >> str;
-		int offset;
 		cout << "Введите число - сдвиг шифра: "; cin >> offset;
 		cin.clear(); while (cin.get() != '\n');
 
-		switch (cipherOperation)
+		switch (operation)
 		{
 		case 'e':
 			cout << "Зашифрованная строка: ";
@@ -28,14 +27,12 @@ int main() {
 			offset = ~offset++;
 		}
 
-		int letterAcode = 0;
-		bool makeOffset = false;
+		int codeA = 0;
 		for (int i = 0; i < strlen; i++) {
-			if (str[i] >= 65 && str[i] <= 90) { letterAcode = 65; }
-			if (str[i] >= 97 && str[i] <= 122) { letterAcode = 97; }
-			if (!((letterAcode & 65) | (letterAcode & 97))) continue;
-			str[i] = ((str[i] - letterAcode % 26 + (offset % 26)) % 26) + letterAcode;
-			letterAcode = 0;
+			codeA = (str[i] >= 97 && str[i] <= 122) ? 97 : (str[i] >= 65 && str[i] <= 90) ? 65 : 0;
+			if (!codeA) continue;
+			str[i] = ((str[i] - codeA % 26 + (offset % 26)) % 26) + codeA;
+			codeA = 0;
 		}
 		cout << str << endl << endl;
 		delete[] str;
